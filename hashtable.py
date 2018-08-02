0e70fe279b4dc9754a9b8c1f0fed1fd932e822f5
Roshan Wagle <roshanwagle@Roshans-MacBook-Pro.local>
2018-08-02 16:37:23 +1000

    Created ashtable

---
+++ hashtable.py
@@ -0,0 +1,18 @@
+"""Creating hashtable."""
+table = [None]*10
+
+
+def hashing_fun(x):
+    """Creating hashfuction."""
+    return x % 10
+
+
+def insert(value):
+    """Inserting value in hashtable."""
+    index = hashing_fun(value)
+    table[index] = value
+    return table
+
+
+input = int(input("Enter the value "))
+print(insert(input))
